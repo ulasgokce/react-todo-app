410a336fe8622420ea50621fc3832545efd2ce7c
ulasgokce <ulasgokce98@gmail.com>
2024-05-10 00:46:05 +0300

    refactor: Remove unused code and comments

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import Another from "./Another";
+function App() {
+  const [count, setCount] = useState(0);
+
+  const someStyle = {
+    color: "red",
+    backgroundColor: "black",
+    fontSize: "50px",
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="">
+          <Another name="my name" />
+          <span>{count}</span>
+          <button onClick={() => setCount(count + 1)}>Increment</button>
+          <button onClick={() => setCount(count - 1)}>Decrement</button>
+        </div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        {true && <p style={someStyle}>{3 + 2}</p>}
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
